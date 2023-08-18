8cac7e9ead85c0f3a8a948171f4b51137633fedc
brian riunge <riungebrian.com>
2023-08-18 16:34:22 +0300

    footer commit

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,8 +1,8 @@
 import Logo from "@/assets/Logo.png"
 
-type Props = {}
 
-const Footer = (props: Props) => {
+
+const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
